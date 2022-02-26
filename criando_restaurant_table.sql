6987aae3ab60c821b5b6f5f7f0fcf15ed296bd3e
Bruno Moreira Pagani <brunompagani@gmail.com>
2022-02-26 17:53:26 -0300

    Cria tabela 'restaraunt' e faz UPSERT

---
+++ criando_restaurant_table.sql
@@ -0,0 +1,93 @@
+USE rest_india.public;
+USE ROLE ACCOUNTADMIN;
+
+---- Sincronizar time zone com horário de São Paulo ----
+ALTER SESSION SET timezone = 'America/Sao_Paulo';
+
+---- Criando objeto File Format considerando array externo ----
+CREATE OR REPLACE FILE FORMAT rest_india.public.json_ff
+    TYPE = JSON
+    STRIP_OUTER_ARRAY = TRUE;
+
+---- Criando stage object que acessa a pasta no bucket S3 ----
+CREATE OR REPLACE STAGE rest_india.public.restaurante_st
+    URL = 's3://challenge-bi-s3/Semana-2/restaurante/'
+    STORAGE_INTEGRATION = semana2_s3_si
+    FILE_FORMAT = rest_india.public.json_ff;
+    
+DESC STAGE rest_india.public.restaurante_st;
+
+LIST @rest_india.public.restaurante_st;
+
+---- Criando Tabela 'restaurant' ----
+CREATE OR REPLACE TABLE rest_india.public.restaurant (
+    rest_id INT NOT NULL,
+    rest_name STRING NOT NULL,
+    country_id INT,
+    city_id INT,
+    city_name STRING,
+    address STRING,
+    locality STRING,
+    locality_verbose STRING,
+    longitude FLOAT,
+    latitude FLOAT,
+    cuisines STRING,
+    avg_cost_for_two INT,
+    currency STRING,
+    has_table_booking BOOLEAN,
+    has_online_delivery BOOLEAN,
+    is_delivering BOOLEAN,
+    switch_to_order_menu BOOLEAN,
+    price_range INT,
+    aggregate_rating FLOAT,
+    rating_color STRING,
+    rating_text STRING,
+    num_of_votes INT,
+    meta_created_ts TIMESTAMP_LTZ DEFAULT CURRENT_TIMESTAMP(),
+    meta_updated_ts TIMESTAMP_LTZ NULL
+);
+
+---- Inserindo primeiros valores na Tabela ----
+INSERT INTO rest_india.public.restaurant (
+    rest_id, rest_name, country_id, city_id, city_name, address, locality, locality_verbose,
+    longitude, latitude, cuisines, avg_cost_for_two, currency, has_table_booking, 
+    has_online_delivery, is_delivering, switch_to_order_menu, price_range, aggregate_rating,
+    rating_color, rating_text, num_of_votes
+)
+    SELECT 
+        rt.value:restaurant:R:res_id::INT AS rest_id,
+        rt.value:restaurant:name::STRING AS rest_name,
+        rt.value:restaurant:location:country_id::INT AS country_id,
+        rt.value:restaurant:location:city_id::INT AS city_id,
+        rt.value:restaurant:location:city::STRING AS city_name,
+        rt.value:restaurant:location:address::STRING AS address,
+        rt.value:restaurant:location:locality::STRING AS locality,
+        rt.value:restaurant:location:locality_verbose::STRING AS locality_verbose,
+        rt.value:restaurant:location:longitude::FLOAT AS longitude,
+        rt.value:restaurant:location:latitude::FLOAT AS latitude,
+        rt.value:restaurant:cuisines::STRING AS cuisines,
+        rt.value:restaurant:average_cost_for_two::INT AS avg_cost_for_two,
+        rt.value:restaurant:currency::STRING AS currency,
+        rt.value:restaurant:has_table_booking::INT::BOOLEAN AS has_table_booking,
+        rt.value:restaurant:has_online_delivery::INT::BOOLEAN AS has_online_delivery,
+        rt.value:restaurant:is_delivering_now::INT::BOOLEAN AS is_delivering,
+        rt.value:restaurant:switch_to_order_menu::INT::BOOLEAN AS switch_to_order_menu,
+        rt.value:restaurant:price_range::INT AS price_range,
+        rt.value:restaurant:user_rating:aggregate_rating::FLOAT AS aggregate_rating,
+        rt.value:restaurant:user_rating:rating_color::STRING AS rating_color,
+        rt.value:restaurant:user_rating:rating_text::STRING AS rating_text,
+        rt.value:restaurant:user_rating:votes::INT AS num_of_votes
+    FROM 
+        @rest_india.public.restaurante_st (PATTERN => '.*file1.json') stg,
+        lateral flatten(input => stg.$1:restaurants) rt
+;
+
+---- See table ----
+SELECT * FROM rest_india.public.restaurant;
+
+---- Check for rest_id duplicates ----
+SELECT rest_id, COUNT(1) AS num_records
+FROM rest_india.public.restaurant
+GROUP BY rest_id
+ORDER BY num_records DESC;
+
---
+++ upsert_script_for_restaurant.sql
@@ -0,0 +1,89 @@
+USE rest_india.public;
+USE ROLE ACCOUNTADMIN;
+---- Sincronizar time zone com horário de São Paulo ----
+ALTER SESSION SET timezone = 'America/Sao_Paulo';
+
+---- Realizando UPSERT de novos valores na tabela com aqruivos subsequentes ----
+
+// Alter this variable to merge new file
+SET FILE_NAME_PATTERN = '.*file5.json';
+
+
+// Create Temp Table as staging area
+CREATE OR REPLACE TEMPORARY TABLE rest_india.public.json_parsed
+AS SELECT 
+        rt.value:restaurant:R:res_id::INT AS rest_id,
+        rt.value:restaurant:name::STRING AS rest_name,
+        rt.value:restaurant:location:country_id::INT AS country_id,
+        rt.value:restaurant:location:city_id::INT AS city_id,
+        rt.value:restaurant:location:city::STRING AS city_name,
+        rt.value:restaurant:location:address::STRING AS address,
+        rt.value:restaurant:location:locality::STRING AS locality,
+        rt.value:restaurant:location:locality_verbose::STRING AS locality_verbose,
+        rt.value:restaurant:location:longitude::FLOAT AS longitude,
+        rt.value:restaurant:location:latitude::FLOAT AS latitude,
+        rt.value:restaurant:cuisines::STRING AS cuisines,
+        rt.value:restaurant:average_cost_for_two::INT AS avg_cost_for_two,
+        rt.value:restaurant:currency::STRING AS currency,
+        rt.value:restaurant:has_table_booking::INT::BOOLEAN AS has_table_booking,
+        rt.value:restaurant:has_online_delivery::INT::BOOLEAN AS has_online_delivery,
+        rt.value:restaurant:is_delivering_now::INT::BOOLEAN AS is_delivering,
+        rt.value:restaurant:switch_to_order_menu::INT::BOOLEAN AS switch_to_order_menu,
+        rt.value:restaurant:price_range::INT AS price_range,
+        rt.value:restaurant:user_rating:aggregate_rating::FLOAT AS aggregate_rating,
+        rt.value:restaurant:user_rating:rating_color::STRING AS rating_color,
+        rt.value:restaurant:user_rating:rating_text::STRING AS rating_text,
+        rt.value:restaurant:user_rating:votes::INT AS num_of_votes
+    FROM 
+        @rest_india.public.restaurante_st (PATTERN => $FILE_NAME_PATTERN) stg,
+        lateral flatten(input => stg.$1:restaurants) rt;
+
+// Deduplicate table
+CREATE OR REPLACE TEMPORARY TABLE rest_india.public.json_parsed_dedup       
+AS SELECT 
+    DISTINCT *
+FROM json_parsed;
+
+// Drop first table
+DROP TABLE rest_india.public.json_parsed;
+
+// Check for rest_id duplicates,,
+-- SELECT rest_id, COUNT(1) AS num_records
+-- FROM rest_india.public.json_parsed_dedup
+-- GROUP BY rest_id
+-- ORDER BY num_records DESC;
+
+// Execute MERGE operation
+MERGE INTO rest_india.public.restaurant tgt
+USING rest_india.public.json_parsed_dedup src
+ON tgt.rest_id = src.rest_id
+WHEN MATCHED THEN UPDATE
+    SET tgt.cuisines = src.cuisines,
+        tgt.avg_cost_for_two = src.avg_cost_for_two,
+        tgt.currency = src.currency,
+        tgt.has_table_booking = src.has_table_booking,
+        tgt.has_online_delivery = src.has_online_delivery,
+        tgt.is_delivering = src.is_delivering,
+        tgt.switch_to_order_menu = src.switch_to_order_menu,
+        tgt.price_range = src.price_range,
+        tgt.aggregate_rating = src.aggregate_rating,
+        tgt.rating_color = src.rating_color,
+        tgt.rating_text = src.rating_text,
+        tgt.num_of_votes = src.num_of_votes,
+        tgt.meta_updated_ts = CURRENT_TIMESTAMP()
+WHEN NOT MATCHED THEN INSERT
+    (rest_id, rest_name, country_id, city_id, city_name, address, locality, locality_verbose,
+     longitude, latitude, cuisines, avg_cost_for_two, currency, has_table_booking, 
+     has_online_delivery, is_delivering, switch_to_order_menu, price_range, aggregate_rating,
+     rating_color, rating_text, num_of_votes
+    ) VALUES
+    (
+    src.rest_id, src.rest_name, src.country_id, src.city_id, src.city_name, src.address, src.locality, src.locality_verbose,
+    src.longitude, src.latitude, src.cuisines, src.avg_cost_for_two, src.currency, src.has_table_booking, 
+    src.has_online_delivery, src.is_delivering, src.switch_to_order_menu, src.price_range, src.aggregate_rating,
+    src.rating_color, src.rating_text, src.num_of_votes
+);
+
+-- DROP TABLE rest_india.public.json_parsed_dedup;
+
+-- SELECT * FROM rest_india.public.restaurant ORDER BY meta_updated_ts;
